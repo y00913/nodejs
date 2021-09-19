3999b7cf9b3023f55a6139ae9ed6f55989b898b3
yutae <ayt0913@gmail.com>
2021-09-19 22:21:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://y00913:y970y913@boilerplate.hyzzm.mongodb.net/boilerplate?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://y00913:passwd@boilerplate.hyzzm.mongodb.net/boilerplate?retryWrites=true&w=majority', {
     useNewUrlParser : true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
